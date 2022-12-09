a4b8bf561bf34d3319d03cab600f8d9f88d8d6fa
yardenfe <117270589+yardenfe@users.noreply.github.com>
2022-12-09 15:54:27 +0200

    last version friday

--- pics/static/project.js
+++ pics/static/project.js
@@ -1,18 +1,40 @@
 
-function empty(){
-  if (document.new_user.email.value==""){
-    alert("some of your fields are empty");
+function empty() {
+  if (document.new_user.email.value == "") {
+    alert("Some of your fields are empty");
+    document.new_user.email.focus();
     return false;
   }
-  if( document.new_user.username.value == "" ) {
-    alert( "Please fill in your first name" );
-    document.NewAccount.fname.focus() ;
+  if (document.new_user.username.value == "") {
+    alert("Some of your fields are empty");
+    document.new_user.username.focus();
     return false;
   }
-  if( document.new_user.password.value == "" ) {
-    alert( "Please fill in your last name" );
-    document.NewAccount.lname.focus() ;
+  if (document.new_user.password.value == "") {
+    alert("Some of your fields are empty");
+    document.new_user.password.focus();
     return false;
   }
-  
+
+  /*-------login page------*/
+  if (document.login.password.value == "") {
+    alert("you must complete all details");
+    document.login.password.focus();
+    return false;
   }
+    if (document.login.username.value == "") {
+    alert("you must complete all details");
+    document.login.username.focus();
+    return false;
+  }
+
+}
+
+function FindTrip() {
+    if (document.new_user.email.value == "") {
+    alert("Some of your fields are empty");
+    document.new_user.email.focus();
+    return false;
+  }
+
+}
